05db852a2f56362a11720643ee98f72c85f08318
pavelmilyukov <Milson7017@yandex.ru>
2024-11-25 20:19:38 +0300

    ok

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,59 +1,23 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.junit.jupiter.api.*;
 import pages.RegistrationPage;
-import pages.helpers.Attach;
-
-import java.util.Map;
+import static io.qameta.allure.Allure.step;
 
 
-public class RegistrationTests extends TestBase {
+class RegistrationTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
 
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-//        Configuration.browser = "chrome";
-        Configuration.timeout = 10000;
-//        Configuration.holdBrowserOpen = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
-
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-
-    }
-
-
-
-
-    @Test
     @Tag("demoqa")
+    @Test
     void fillFormTest() {
-        registrationPage.openPage()
+        step("Открыть страницу с формой", () -> {
+                registrationPage.openPage();
+        });
+        step("Заполнить поля", () -> {
+        registrationPage
                 .cleanBanner()
                 .setFirstName("Pavel")
                 .setLastName("Milyukov")
@@ -68,7 +32,10 @@ public class RegistrationTests extends TestBase {
                 .setState("NCR")
                 .setCity("Noida")
                 .submit();
+        });
 
+
+        step("Проверка на заполнение полей", () -> {
         registrationPage
                 .checkTableResponse("Student Name", "Pavel Milyukov")
                 .checkTableResponse("Student Email", "milyukov@yandex.ru")
@@ -80,37 +47,50 @@ public class RegistrationTests extends TestBase {
                 .checkTableResponse("Picture", "bag.png")
                 .checkTableResponse("Address", "ул.Ленина")
                 .checkTableResponse("State and City", "NCR Noida");
+        });
     }
 
-    @Test
+
     @Tag("demoqa")
+    @Test
     void minimumAmountData() {
-        registrationPage.openPage()
-                .cleanBanner()
-                .setFirstName("Pavel")
-                .setLastName("Milyukov")
-                .setUserEmail("milyukov@yandex.ru")
-                .setGender("Male")
-                .setNumber("0123456789")
-                .setDateOfBirth("16", "January", "1993")
-                .setHobbies("Sports")
-                .setAddress("ул.Ленина")
-                .submit();
+        step("Открыть страницу с формой", () -> {
+            registrationPage.openPage();
+        });
+        step("Заполнить поля", () -> {
+            registrationPage
+                    .cleanBanner()
+                    .setFirstName("Pavel")
+                    .setLastName("Milyukov")
+                    .setUserEmail("milyukov@yandex.ru")
+                    .setGender("Male")
+                    .setNumber("0123456789")
+                    .setDateOfBirth("16", "January", "1993")
+                    .setHobbies("Sports")
+                    .setAddress("ул.Ленина")
+                    .submit();
+        });
 
-        registrationPage
-                .checkTableResponse("Student Name", "Pavel Milyukov")
-                .checkTableResponse("Student Email", "milyukov@yandex.ru")
-                .checkTableResponse("Gender", "Male")
-                .checkTableResponse("Mobile", "0123456789")
-                .checkTableResponse("Date of Birth", "16 January,1993")
-                .checkTableResponse("Hobbies", "Sports")
-                .checkTableResponse("Address", "ул.Ленина");
+        step("Проверка на заполнение полей", () -> {
+            registrationPage
+                    .checkTableResponse("Student Name", "Pavel Milyukov")
+                    .checkTableResponse("Student Email", "milyukov@yandex.ru")
+                    .checkTableResponse("Gender", "Male")
+                    .checkTableResponse("Mobile", "0123456789")
+                    .checkTableResponse("Date of Birth", "16 January,1993")
+                    .checkTableResponse("Hobbies", "Sports")
+                    .checkTableResponse("Address", "ул.Ленина");
+        });
     }
 
-    @Test
     @Tag("demoqa")
+    @Test
     void incorrectPhoneNumberTest() {
-        registrationPage.openPage()
+        step("Открыть страницу с формой", () -> {
+            registrationPage.openPage();
+        });
+        step("Заполнить поля", () -> {
+        registrationPage
                 .cleanBanner()
                 .setFirstName("FirstName")
                 .setLastName("LastName")
@@ -118,8 +98,9 @@ public class RegistrationTests extends TestBase {
                 .setNumber("5553535")
                 .setDateOfBirth("17", "July", "2003")
                 .submit();
-
+        });
+        step("Негативная проверка", () -> {
         registrationPage.checkResultIsNotVisible();
-
+        });
     }
 }
